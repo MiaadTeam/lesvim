fb5035e88cc691e8b0881cdf2b57613f9322294b
Syd Amir <hemedani@gmail.com>
2021-08-10 13:12:43 +0430

    add some nav

--- lua/keymap/global.lua
+++ lua/keymap/global.lua
@@ -4,6 +4,16 @@
 -- copy past on neovide
 map('i', '<D-v>', '<C-r>+', mapOptions)
 
+-- Better nav for omnicomplete
+vim.cmd('inoremap <expr> <c-j> (\"\\<C-n>\")')
+vim.cmd('inoremap <expr> <c-k> (\"\\<C-p>\")')
+
+-- fix to get netrw's gx command to work correctly
+map('n', 'gx', ":call netrw#BrowseX(expand((exists('g:netrw_gx')? g:netrw_gx : '<cfile>')),netrw#CheckIfRemote())<cr>", {noremap = true, silent = true})
+
+vim.cmd('vnoremap p "0p')
+vim.cmd('vnoremap P "0P')
+
 return {
  map = map,
  mapOptions = mapOptions
