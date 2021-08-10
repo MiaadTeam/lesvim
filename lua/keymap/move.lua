e708e48f842ace28c1087401fbac05df9832bee5
Syd Amir <hemedani@gmail.com>
2021-08-10 13:15:25 +0430

    bind move selected line

---
+++ lua/keymap/move.lua
@@ -0,0 +1,7 @@
+local global = require("keymap.global")
+ local map = global.map
+ mapOptions = global.mapOptions
+
+-- Move selected line / block of text in visual mode
+map('x', 'K', ':move \'<-2<CR>gv-gv', mapOptions)
+map('x', 'J', ':move \'>+1<CR>gv-gv', mapOptions)
