689cb89f9c7f5bacd07a2c0960324fc4ef15e63e
Syd Amir <hemedani@gmail.com>
2021-11-19 22:40:18 +0330

    config lua snip

--- lua/packer/plugins.lua
+++ lua/packer/plugins.lua
@@ -201,7 +201,12 @@ require("packer").startup(function()
 	use("hrsh7th/cmp-buffer")
 	use("hrsh7th/cmp-path")
 	use("hrsh7th/cmp-cmdline")
-	use("L3MON4D3/LuaSnip")
+	use({
+		"L3MON4D3/LuaSnip",
+		config = function()
+			require("packer.settings.luasnip")
+		end,
+	})
 	use({
 		"hrsh7th/nvim-cmp",
 		requires = {
@@ -215,6 +220,13 @@ require("packer").startup(function()
 
 		-- event = "InsertEnter",
 		config = function()
+			local has_words_before = function()
+				local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+				return col ~= 0
+					and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+			end
+
+			local luasnip = require("luasnip")
 			local cmp = require("cmp")
 			local lspkind = require("lspkind")
 
@@ -229,8 +241,36 @@ require("packer").startup(function()
 					["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
 					["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
 
-					["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "s" }),
-					["<S-Tab>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "s" }),
+					["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						elseif luasnip.expand_or_jumpable() then
+							luasnip.expand_or_jump()
+						elseif has_words_before() then
+							cmp.complete()
+						else
+							fallback()
+						end
+					end, {
+						"i",
+						"s",
+					}),
+
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						elseif luasnip.jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end, {
+						"i",
+						"s",
+					}),
+
+					-- ["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "s" }),
+					-- ["<S-Tab>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "s" }),
 
 					["<C-y>"] = cmp.config.disable, -- If you want to remove the default `<C-y>` mapping, You can specify `cmp.config.disable` value.
 					["<C-e>"] = cmp.mapping({
