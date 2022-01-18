804f259dd574c11535907a425256c60210e1ad4b
Syd Amir <hemedani@gmail.com>
2022-01-18 13:59:24 +0330

    add some rust config

--- lua/lsp/config/rust.lua
+++ lua/lsp/config/rust.lua
@@ -2,4 +2,42 @@ local setup = require("lsp.lsp-setup")
 
 require("lspconfig").rust_analyzer.setup({
 	capabilities = setup.capabilities,
+	settings = {
+		["rust-analyzer"] = {
+			assist = {
+				importGranularity = "module",
+				importPrefix = "by_self",
+			},
+			cargo = {
+				loadOutDirsFromCheck = true,
+			},
+			procMacro = {
+				enable = true,
+			},
+		},
+	},
 })
+
+-- local nvim_lsp = require("lspconfig")
+--
+-- local on_attach = function(client)
+-- 	require("completion").on_attach(client)
+-- end
+--
+-- nvim_lsp.rust_analyzer.setup({
+-- 	on_attach = on_attach,
+-- 	settings = {
+-- 		["rust-analyzer"] = {
+-- 			assist = {
+-- 				importGranularity = "module",
+-- 				importPrefix = "by_self",
+-- 			},
+-- 			cargo = {
+-- 				loadOutDirsFromCheck = true,
+-- 			},
+-- 			procMacro = {
+-- 				enable = true,
+-- 			},
+-- 		},
+-- 	},
+-- })
