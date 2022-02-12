fd8f05f5261e4082eca4cdb10b9e5542d22401c0
Syd Amir <hemedani@gmail.com>
2022-02-12 19:06:54 +0330

    fixed setup autotag

--- lua/packer/plugins.lua
+++ lua/packer/plugins.lua
@@ -40,7 +40,12 @@ require("packer").startup(function()
 		end,
 	})
 
-	use({ "windwp/nvim-ts-autotag" })
+	use({
+		"windwp/nvim-ts-autotag",
+		config = function()
+			require("nvim-ts-autotag").setup()
+		end,
+	})
 
 	use({
 		"windwp/nvim-spectre",
