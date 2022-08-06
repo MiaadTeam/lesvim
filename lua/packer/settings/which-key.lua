a575407af0eeda7e92d880b96b06e1080f84d42c
Syd Amir <hemedani@gmail.com>
2022-08-06 20:12:56 +0430

    :art: improve packer and which key

--- lua/packer/settings/which-key.lua
+++ lua/packer/settings/which-key.lua
@@ -23,15 +23,17 @@ M.settings = {
 		group = "+", -- symbol prepended to a group
 	},
 	window = {
-		border = "single", -- none, single, double, shadow
+		border = "rounded", -- none, single, double, shadow
 		position = "bottom", -- bottom, top
 		margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
 		padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
+		winblend = 0,
 	},
 	layout = {
 		height = { min = 4, max = 25 }, -- min and max height of the columns
 		width = { min = 20, max = 50 }, -- min and max width of the columns
 		spacing = 3, -- spacing between columns
+		align = "left",
 	},
 	hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
 	show_help = true, -- show help message on the command line when the popup is visible
