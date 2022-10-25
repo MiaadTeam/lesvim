9b09d88fe8298f8946b64205aba6b714b08158e0
Syd Amir <hemedani@gmail.com>
2022-10-25 14:49:05 +0330

    :wrench: add config for biscuts plugin

--- lua/packer/plugins.lua
+++ lua/packer/plugins.lua
@@ -464,7 +464,24 @@ return packer.startup(function(use)
     "code-biscuits/nvim-biscuits",
 
     config = function()
-      require("nvim-biscuits").setup({})
+      require("nvim-biscuits").setup({
+        ensure_installed = "maintained",
+
+        default_config = {
+          max_length = 12,
+          min_distance = 5,
+          prefix_string = " 📎 ",
+        },
+        language_config = {
+          html = {
+            prefix_string = " 🌐 ",
+          },
+          javascript = {
+            prefix_string = " ✨ ",
+            max_length = 80,
+          },
+        },
+      })
     end,
     requires = {
       { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" },
