84f7b9ba72e8f0cff42a2aeb9770679bdd939c7c
Syd Amir <hemedani@gmail.com>
2022-10-25 14:45:33 +0330

    :wrench: add config for hls lense

--- lua/packer/plugins.lua
+++ lua/packer/plugins.lua
@@ -362,7 +362,35 @@ return packer.startup(function(use)
       require("packer.settings.rest")
     end,
   })
-  use({ "kevinhwang91/nvim-hlslens" })
+
+  -- Hlsearch Lens for Neovim
+  use({
+    "kevinhwang91/nvim-hlslens",
+    config = function()
+      require("hlslens").setup()
+
+      local kopts = { noremap = true, silent = true }
+
+      vim.api.nvim_set_keymap(
+        "n",
+        "n",
+        [[<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>]],
+        kopts
+      )
+      vim.api.nvim_set_keymap(
+        "n",
+        "N",
+        [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>]],
+        kopts
+      )
+      vim.api.nvim_set_keymap("n", "*", [[*<Cmd>lua require('hlslens').start()<CR>]], kopts)
+      vim.api.nvim_set_keymap("n", "#", [[#<Cmd>lua require('hlslens').start()<CR>]], kopts)
+      vim.api.nvim_set_keymap("n", "g*", [[g*<Cmd>lua require('hlslens').start()<CR>]], kopts)
+      vim.api.nvim_set_keymap("n", "g#", [[g#<Cmd>lua require('hlslens').start()<CR>]], kopts)
+
+      vim.api.nvim_set_keymap("n", "<Leader>l", ":noh<CR>", kopts)
+    end,
+  })
 
   use({
     "simrat39/rust-tools.nvim",
