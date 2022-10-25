6983e9ff8413b736e2855a0fea6a63e8602426a7
Syd Amir <hemedani@gmail.com>
2022-10-25 14:35:27 +0330

    :wastebasket: replace deprecate code

--- lua/lsp/lsp-setup.lua
+++ lua/lsp/lsp-setup.lua
@@ -1,4 +1,4 @@
 local M = {}
-M.capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+M.capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 return M
