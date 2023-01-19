bf64a4d77bbc85bbab3e38e67b49dd93853c798c
Syd Amir <hemedani@gmail.com>
2023-01-19 10:21:18 +0330

    :art: some nice active blankline highlight

--- lua/packer/settings/blankline.lua
+++ lua/packer/settings/blankline.lua
@@ -13,25 +13,27 @@ vim.cmd([[highlight IndentBlanklineIndent3 guibg=#2f2a3b gui=nocombine]])
 vim.cmd([[highlight IndentBlanklineIndent4 guibg=#262933 gui=nocombine]])
 
 vim.opt.list = true
--- vim.opt.listchars:append("space:—")
+vim.opt.listchars:append("space:⋅")
 vim.opt.listchars:append("eol:↴")
 
 require("indent_blankline").setup({
-	space_char_blankline = " ",
-	char_highlight_list = {
-		"IndentHighlightListBlank1",
-		"IndentHighlightListBlank2",
-		"IndentHighlightListBlank3",
-		"IndentHighlightListBlank4",
-		"IndentHighlightListBlank5",
-		"IndentHighlightListBlank6",
-	},
+  space_char_blankline = " ",
+  show_trailing_blankline_indent = true,
+  show_current_context = true,
+  show_current_context_start = true,
+  char_highlight_list = {
+    "IndentHighlightListBlank1",
+    "IndentHighlightListBlank2",
+    "IndentHighlightListBlank3",
+    "IndentHighlightListBlank4",
+    "IndentHighlightListBlank5",
+    "IndentHighlightListBlank6",
+  },
 
-	space_char_highlight_list = {
-		"IndentBlanklineIndent1",
-		"IndentBlanklineIndent2",
-		"IndentBlanklineIndent3",
-		"IndentBlanklineIndent4",
-	},
-	show_trailing_blankline_indent = true,
+  space_char_highlight_list = {
+    "IndentBlanklineIndent1",
+    "IndentBlanklineIndent2",
+    "IndentBlanklineIndent3",
+    "IndentBlanklineIndent4",
+  },
 })
