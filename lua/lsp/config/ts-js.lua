43a2106b3b42e8ebb9320c10453fcec6fa2ffaec
Syd Amir <hemedani@gmail.com>
2023-04-13 15:05:42 +0330

    :wrench: update lsp config for deno and tsserver

--- lua/lsp/config/ts-js.lua
+++ lua/lsp/config/ts-js.lua
@@ -1,53 +1,99 @@
 -- local on_attach = require("lsp.lsp-setup").on_attach
 local setup = require("lsp.lsp-setup")
+local nvim_lsp = require("lspconfig")
 
 local function file_exists(name)
-	local f = io.open(name, "r")
-	if f ~= nil then
-		io.close(f)
-		return true
-	else
-		return false
-	end
+  local f = io.open(name, "r")
+  if f ~= nil then
+    io.close(f)
+    return true
+  else
+    return false
+  end
 end
 
-if file_exists(os.getenv("PWD") .. "/package.json") or file_exists(os.getenv("PWD") .. "/tsconfig.json") then
-	require("lspconfig").tsserver.setup({
-		capabilities = setup.capabilities,
-
-		-- autostart = true,
-
-		on_attach = require("lsp.lsp-attach").on_attach, -- This makes sure tsserver is not used for formatting (I prefer prettier)
-		-- on_attach = require'lsp'.common_on_attach,
-		-- root_dir = require("lspconfig/util").root_pattern("package.json", "tsconfig.json", "jsconfig.json", ".git"),
-		settings = { documentFormatting = false },
-		-- handlers = {
-		-- 	["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-		-- 		virtual_text = O.tsserver.diagnostics.virtual_text,
-		-- 		signs = O.tsserver.diagnostics.signs,
-		-- 		underline = O.tsserver.diagnostics.underline,
-		-- 		update_in_insert = true,
-		-- 	}),
-		-- },
-	})
+if file_exists(os.getenv("PWD") .. "/deno.json") or file_exists(os.getenv("PWD") .. "/deno.jsonc") then
+  nvim_lsp.denols.setup({
+    capabilities = setup.capabilities,
+    -- autostart = true,
+    cmd = { "deno", "lsp" },
+    on_attach = require("lsp.lsp-attach").on_attach,
+    filetypes = {
+      "javascript",
+      "javascriptreact",
+      "javascript.jsx",
+      "typescript",
+      "typescriptreact",
+      "typescript.tsx",
+    },
+    init_options = {
+      enable = true,
+      lint = true,
+      unstable = true,
+    },
+    root_dir = nvim_lsp.util.root_pattern("deno.json", "deno.jsonc"),
+  })
 else
-	require("lspconfig").denols.setup({
-		capabilities = setup.capabilities,
-		-- autostart = true,
-		cmd = { "deno", "lsp" },
-		on_attach = require("lsp.lsp-attach").on_attach,
-		filetypes = {
-			"javascript",
-			"javascriptreact",
-			"javascript.jsx",
-			"typescript",
-			"typescriptreact",
-			"typescript.tsx",
-		},
-		init_options = {
-			enable = true,
-			lint = true,
-			unstable = true,
-		},
-	})
+  nvim_lsp.tsserver.setup({
+    capabilities = setup.capabilities,
+
+    -- autostart = true,
+
+    on_attach = require("lsp.lsp-attach").on_attach, -- This makes sure tsserver is not used for formatting (I prefer prettier)
+    -- on_attach = require'lsp'.common_on_attach,
+    -- root_dir = require("lspconfig/util").root_pattern("package.json", "tsconfig.json", "jsconfig.json", ".git"),
+    settings = { documentFormatting = false },
+    -- handlers = {
+    -- 	["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+    -- 		virtual_text = O.tsserver.diagnostics.virtual_text,
+    -- 		signs = O.tsserver.diagnostics.signs,
+    -- 		underline = O.tsserver.diagnostics.underline,
+    -- 		update_in_insert = true,
+    -- 	}),
+    -- },
+    root_dir = nvim_lsp.util.root_pattern("package.json"),
+    -- single_file_support = false,
+  })
 end
+
+-- nvim_lsp.denols.setup({
+--   capabilities = setup.capabilities,
+--   -- autostart = true,
+--   cmd = { "deno", "lsp" },
+--   on_attach = require("lsp.lsp-attach").on_attach,
+--   filetypes = {
+--     "javascript",
+--     "javascriptreact",
+--     "javascript.jsx",
+--     "typescript",
+--     "typescriptreact",
+--     "typescript.tsx",
+--   },
+--   init_options = {
+--     enable = true,
+--     lint = true,
+--     unstable = true,
+--   },
+--   root_dir = nvim_lsp.util.root_pattern("deno.json", "deno.jsonc"),
+-- })
+--
+-- nvim_lsp.tsserver.setup({
+--   capabilities = setup.capabilities,
+--
+--   -- autostart = true,
+--
+--   on_attach = require("lsp.lsp-attach").on_attach, -- This makes sure tsserver is not used for formatting (I prefer prettier)
+--   -- on_attach = require'lsp'.common_on_attach,
+--   -- root_dir = require("lspconfig/util").root_pattern("package.json", "tsconfig.json", "jsconfig.json", ".git"),
+--   settings = { documentFormatting = false },
+--   -- handlers = {
+--   -- 	["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+--   -- 		virtual_text = O.tsserver.diagnostics.virtual_text,
+--   -- 		signs = O.tsserver.diagnostics.signs,
+--   -- 		underline = O.tsserver.diagnostics.underline,
+--   -- 		update_in_insert = true,
+--   -- 	}),
+--   -- },
+--   root_dir = nvim_lsp.util.root_pattern("package.json"),
+--   single_file_support = false,
+-- })
