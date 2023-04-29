0886e3957d30be2f0463e3c3643cd86f4b7582ad
Syd Amir <hemedani@gmail.com>
2023-04-29 16:39:42 +0330

    :ambulance: make < > shifts keep selection

--- lua/keymap/move.lua
+++ lua/keymap/move.lua
@@ -5,3 +5,7 @@ local mapOptions = global.mapOptions
 -- Move selected line / block of text in visual mode
 map("x", "K", ":move '<-2<CR>gv-gv", mapOptions)
 map("x", "J", ":move '>+1<CR>gv-gv", mapOptions)
+
+-- make < > shifts keep selection
+map("v", "<", "<gv", mapOptions)
+map("v", ">", ">gv", mapOptions)
